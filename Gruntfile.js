0ea1a166e9d2ca6f0ca2065bf34b8c6936eb84a8
Radek Stepan <radek@redmantech.com>
2014-10-08 09:18:07 -0600

    simplified

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,39 +3,28 @@ module.exports = function(grunt) {
   grunt.initConfig({
     'pkg': grunt.file.readJSON('package.json'),
 
-    'clean': [ 'dist/' ],
+    'clean': [ 'build/' ],
 
     'sass': {
-      dist: {
-        options: {
-          loadPath: [ '/.scss' ],
-          bundleExec: true
+      'dist': {
+        'options': {
+          'loadPath': [ '/.src' ],
+          'bundleExec': true
         },
-        files: {
-          'dist/assets/css/foundation.css': 'scss/foundation.scss',
-          'dist/assets/css/normalize.css': 'scss/normalize.scss'
+        'files': {
+          'build/foundation.css': 'src/foundation.scss',
+          'build/normalize.css': 'src/normalize.scss'
         }
       }
     },
 
-    'copy': {
-      dist: {
-        files: [
-          { 'expand': true, 'cwd': 'doc/assets/', 'src': ['**/*','!{scss}/**/*'], 'dest': 'dist/docs/assets/', 'filter': 'isFile' },
-          { 'expand': true, 'cwd': 'scss/', 'src': '**/*.scss', 'dest': 'dist/assets/scss/', 'filter': 'isFile' }
-        ]
-      }
-    },
-
     'string-replace': {
-      dist: {
-        files: {
-          'dist/assets/css/':'dist/assets/css/*.css',
-          'dist/assets/scss/foundation/components/':'dist/assets/scss/foundation/components/*.scss',
-          'dist/docs/assets/css/':'dist/docs/assets/css/*.css'
+      'dist': {
+        'files': {
+          'build/':'build/*.css'
         },
-        options: {
-          replacements: [
+        'options': {
+          'replacements': [
             { 'pattern': /{{\s*VERSION\s*}}/g, 'replacement': '<%= pkg.version %>' }
           ]
         }
@@ -46,10 +35,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jst');
   grunt.loadNpmTasks('grunt-string-replace');
 
-  grunt.registerTask('default', [ 'clean', 'sass', 'copy', 'string-replace' ]);
+  grunt.registerTask('default', [ 'clean', 'sass', 'string-replace' ]);
 
 };
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [Foundation](http://foundation.zurb.com)
+#[Foundation](http://foundation.zurb.com)
 
 Prefixed Foundation CSS framework.
 
@@ -8,3 +8,8 @@ $ bundle install
 $ npm install
 $ grunt build
 ```
+
+##Changes
+
+[ ] A `foundation` class prefix on all rules
+[ ] No `max-width` on grid rows
